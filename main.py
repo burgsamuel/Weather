4be1bf62d0c16ac5a463e2437852d450748275cf
burgsamuel <57895180+burgsamuel@users.noreply.github.com>
2024-11-15 16:34:36 +1100

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def horse_data(data: dict = Body (...)):
 
 @app.get("/horsedata")
 async def horsedata():
-    data = horse_data_list    
+    data = await horse_data_list    
     return data
 
 
